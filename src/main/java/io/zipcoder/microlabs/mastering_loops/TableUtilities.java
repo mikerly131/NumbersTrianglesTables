5315f1bc5f784e71f965533c487c21a0a167e041
Mike <mike@Zipcoders-MacBook-Pro-83.local>
2023-10-30 18:50:52 -0400

    finished

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,15 +1,29 @@
 package io.zipcoder.microlabs.mastering_loops;
 
+import static io.zipcoder.microlabs.mastering_loops.NumberUtilities.getSquareNumbers;
+
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
-    }
+        return getMultiplicationTable(5); }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        // going to need to produce a square of size tableSize * tableSize;
+        StringBuilder multTable = new StringBuilder();
+        // loop to produce columns
+
+        for (int i = 1; i <= tableSize; i++) {
+            // loop to fill out row
+            for (int j = 1; j <= tableSize; j++) {
+                 multTable.append(String.format("%3d |", i * j));
+            }
+            // new ling
+            multTable.append("\n");
+        }
+
+        return multTable.toString();
     }
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,27 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        StringBuilder triangle = new StringBuilder();
+        for (int i = 1; i < numberOfRows; i++) {
+            triangle.append(getRow(i));
+            triangle.append("\n");
+        }
+        return triangle.toString();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        StringBuilder rows = new StringBuilder();
+        for (int i = 0; i < numberOfStars; i++) {
+            rows.append("*");
+        }
+        return rows.toString();
     }
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
     }
 }
